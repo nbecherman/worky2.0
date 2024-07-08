b69a8127dc29ba91d61bf919125d7031925e2118
NICOLÁS DAVID BECHERMAN <47700068@ort>
2024-07-08 11:53:40 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+
 import './css/App.css';
 import Horario from './componentes/Horario';
 import CrearServicio from './componentes/CreateService';
+import Index from  './componentes/Index';
+import Navbar from  './componentes/Navbar';
+
+
 function App() {
   return (
     <>
+    <Index/>
     <CrearServicio/>
-      <Horario />
+    <Horario /> 
+    <Navbar /> 
+
       
     </>
   );
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateService from './componentes/CreateService';
 import Horario from './componentes/Horario';
+import Navbar from './componentes/Navbar';
+import Index from './componentes/Index';
+
 
 const AppRouter = () => (
   <Router>
     <Routes>
       <Route path="/" element={<CreateService />} />
       <Route path="/horario" element={<Horario />} />
+      <Route path="/Navbar" element={<Navbar />} />
+      <Route path="/Index" element={<Index />} />
+
     </Routes>
   </Router>
 );
